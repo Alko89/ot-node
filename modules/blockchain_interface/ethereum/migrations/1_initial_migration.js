8b36833d41b03e932d4ccdd80bf92c4a0c9eb1ae
radomir.sebek <rasho.ieee@gmail.com>
2018-03-28 21:58:28 +0200

    [ganache-cli] Setup blockchain testing framework in ot-node project
    - initial commit

--- modules/blockchain_interface/ethereum/migrations/1_initial_migration.js
+++ modules/blockchain_interface/ethereum/migrations/1_initial_migration.js
@@ -1,6 +1,6 @@
-let artifacts;
-const Migrations = artifacts.require('./Migrations.sol');
+var Migrations = artifacts.require('Migrations'); // eslint-disable-line no-undef
 
-module.exports = (deployer) => {
+module.exports = function (deployer) {
+    // Deploy the Migrations contract as our only task
     deployer.deploy(Migrations);
 };
