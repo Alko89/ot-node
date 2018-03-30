b9085da3e9948dd27b9dcdeced873420c652b5d2
Vladimir <v.lelicanin@sae.edu>
2018-03-30 11:14:38 +0200

    setup

---
+++ ot-node.js
@@ -0,0 +1,59 @@
+const Network = require('./modules/Network');
+const Utilities = require('./modules/utilities');
+
+
+const log = Utilities.getLogger();
+
+var config;
+var graphDB;
+
+
+class OTNode {
+
+    bootstrap() {
+        return new Promise((resolve, reject) => {
+            Utilities.loadConfig().then((response) => {
+                config = Utilities.getConfig();
+                graphDB = new GraphStorage();
+
+                graphDB.connect().then((response) => {
+                    resolve('OK');
+
+                }).catch((err) => {
+                    console.log(err);
+                    reject(err);
+                });
+            }).catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+        });
+    }
+}
+
+const otNode = new OTNode();
+otNode.bootstrap().then((response) => {
+    console.log(response);
+    console.log(config);
+    console.log(graphDB);
+}).catch((err) => {
+    console.log(err);
+})
+
+/*
+const graphDB = new GraphStorage();
+graphDB.connect().then((response) => {
+    //  console.log(response);
+    graphDB.addDocument('ot_vertices', { _key: '123' }).then(() => {
+        graphDB.runQuery('FOR v IN ot_vertices RETURN v._key').then((result) => {
+            console.log(result);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }).catch((err) => {
+        console.log(err);
+    });
+}).catch((err) => {
+    console.log(err);
+});
+*/
